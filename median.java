0c5a0b48f583bfdad51b5ac6e12bf995ae572a50
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-11-18 16:20:22 -0500

    Create median.java

---
+++ median.java
@@ -0,0 +1,35 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2) return  findMedianSortedArrays(nums2,nums1);
+
+        int px,py;
+        int low=0,high=n1;
+        while(low<=high){
+            px=low+(high-low)/2;
+            py=(n1+n2)/2-px;
+
+            double l1=px==0?Integer.MIN_VALUE:nums1[px-1];
+            double l2=py==0?Integer.MIN_VALUE:nums2[py-1];
+            double r1=px==n1?Integer.MAX_VALUE:nums1[px];
+            double r2=py==n2?Integer.MAX_VALUE:nums2[py];
+
+            if(l1<=r2 && l2<=r1){
+                if((n1+n2)%2==0){
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2;
+                }
+                else{
+                    return Math.min(r1,r2);
+                }
+            }
+            else if(l2>r1){
+                low=px+1;;
+            }
+            else{
+                high=px-1;
+            }
+        }
+        return 0.11;
+    }
+}
